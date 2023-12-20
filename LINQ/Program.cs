df22f4cb84f2d0febf3a81492782d44f49d6f076
Corey Schroeder <codingcorey365@gmail.com>
2023-12-19 18:32:50 -0600

    commit after saveing :D

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -2,9 +2,51 @@
 {
     public class Program
     {
+
+        //Remember to use LINQ which involves using a lambda expression.
+        //Use the list of common LINQ methods on the lecture page for help on deciding which method(s) to use:
+        //Linq Methods
+        //Use Method Syntax for this exercise.
+        //Stage, Commit, and Push your work to Github.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Miah!");
+            //Create a list of type string.
+            List<string> gameList = new List<string>();
+
+            //Populate the list with video game names.
+            gameList.Add("test");
+            gameList.Add("The Elder Scrolls V: Skyrim");
+            gameList.Add("Minecraft");
+            gameList.Add("The Witcher 3: Wild Hunt");
+            gameList.Add("World of Warcraft");
+            gameList.Add("Doom");
+            gameList.Add("Half - Life 2");
+            gameList.Add("Portal 2");
+            gameList.Add("Dark Souls");
+            gameList.Add("BioShock");
+            gameList.Add("The Legend of Zelda: Breath of the Wild");
+            gameList.Add("Grand Theft Auto V");
+            gameList.Add("Red Dead Redemption 2");
+            gameList.Add("Fallout 4");
+            gameList.Add("Mass Effect 2");
+            gameList.Add("Diablo III");
+            gameList.Add("League of Legends");
+            gameList.Add("Fortnite");
+            gameList.Add("Overwatch");
+            gameList.Add("The Last of Us");
+            gameList.Add("God of War");
+            gameList.Add("Counter - Strike: Global Offensive");
+            gameList.Add("Civilization VI");
+            gameList.Add("Assassin's Creed Odyssey");
+            gameList.Add("Rocket League");
+            gameList.Add("Stardew Valley");
+            gameList.Add("Celeste");
+            gameList.Add("Hollow Knight");
+            gameList.Add("Sekiro: Shadows Die Twice");
+            gameList.Add("Divinity: Original Sin 2");
+            gameList.Add("Hades");
+            
+            //Order the list of games by the length of the game name.
         }
     }
 }
